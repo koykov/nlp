bcfa8ecf0f85387c0bd4bc230d483e59aebdbc15
Konstantin Voykov <koykov0@gmail.com>
2022-01-12 23:52:07 +0200

    LD issue: improve signature.

--- language_detect.go
+++ language_detect.go
@@ -1,6 +1,6 @@
 package nlp
 
 type LanguageDetector interface {
-	Detect(text, def string) Language
-	DetectName(text, def string) string
+	Detect(text []byte, def Language) Language
+	DetectString(text string, def Language) Language
 }
