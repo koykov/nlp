4982081921e69196b5527351c01c088db9935069
Konstantin Voykov <koykov0@gmail.com>
2022-01-22 22:21:42 +0200

    nlpc issue: use separate sources for languages/scripts modules.

--- generate.go
+++ generate.go
@@ -1,4 +1,4 @@
 package nlp
 
-//go:generate nlpc --module=languages --input=languages.json --target=languages_repo.go
-//go:generate nlpc --module=scripts --input=languages.json --target=scripts_repo.go
+//go:generate nlpc --module=languages --input=raw/languages.json --target=languages_repo.go
+//go:generate nlpc --module=scripts --input=raw/scripts.json --target=scripts_repo.go
