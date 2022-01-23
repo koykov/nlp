270867a3cfe77b17e3a91669c5b6f4de98ba6b19
Konstantin Voykov <koykov0@gmail.com>
2022-01-23 23:00:51 +0200

    Fix language names in tests.

--- language_test.go
+++ language_test.go
@@ -8,38 +8,38 @@ func TestLanguage(t *testing.T) {
 			t.Errorf("assertion failed: need '%s', got '%s'", e, x)
 		}
 	}
-	t.Run("name", func(t *testing.T) { assert(t, Russian.String(), "Russian") })
-	t.Run("native", func(t *testing.T) { assert(t, Russian.Native(), "Русский") })
-	t.Run("iso639-1", func(t *testing.T) { assert(t, Russian.Iso6391(), "ru") })
-	t.Run("iso639-3", func(t *testing.T) { assert(t, Russian.Iso6393(), "rus") })
+	t.Run("name", func(t *testing.T) { assert(t, LanguageRussian.String(), "Russian") })
+	t.Run("native", func(t *testing.T) { assert(t, LanguageRussian.Native(), "Русский") })
+	t.Run("iso639-1", func(t *testing.T) { assert(t, LanguageRussian.Iso6391(), "ru") })
+	t.Run("iso639-3", func(t *testing.T) { assert(t, LanguageRussian.Iso6393(), "rus") })
 }
 
 func BenchmarkLanguage(b *testing.B) {
 	b.Run("name", func(b *testing.B) {
 		var x string
 		for i := 0; i < b.N; i++ {
-			x = Russian.String()
+			x = LanguageRussian.String()
 		}
 		_ = x
 	})
 	b.Run("native", func(b *testing.B) {
 		var x string
 		for i := 0; i < b.N; i++ {
-			x = Russian.Native()
+			x = LanguageRussian.Native()
 		}
 		_ = x
 	})
 	b.Run("iso639-1", func(b *testing.B) {
 		var x string
 		for i := 0; i < b.N; i++ {
-			x = Russian.Iso6391()
+			x = LanguageRussian.Iso6391()
 		}
 		_ = x
 	})
 	b.Run("iso639-3", func(b *testing.B) {
 		var x string
 		for i := 0; i < b.N; i++ {
-			x = Russian.Iso6393()
+			x = LanguageRussian.Iso6393()
 		}
 		_ = x
 	})
