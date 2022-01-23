a60e56a6d5ec57f1003b32f77d70c3ac782c25ef
Konstantin Voykov <koykov0@gmail.com>
2022-01-23 23:46:13 +0200

    Draft of script's rune evaluator.

--- script.go
+++ script.go
@@ -1,5 +1,7 @@
 package nlp
 
+import "unicode"
+
 type Script uint
 
 func (s Script) Evaluate(p []byte) bool {
@@ -27,7 +29,96 @@ func (s Script) EvaluateByte(b byte) bool {
 }
 
 func (s Script) EvaluateRune(r rune) bool {
-	// todo implement
-	_ = r
+	t, ok := s.gets()
+	if !ok {
+		return false
+	}
+	return unicode.Is(t, r)
+}
+
+func (s Script) gets() (*unicode.RangeTable, bool) {
+	if int(s) >= len(__st_buf) {
+		return nil, false
+	}
+	return __st_buf[s], true
+}
+
+// EvaluateRune1 is testing precompiled version of Han script todo remove me
+func (s Script) EvaluateRune1(r rune) bool {
+	t := unicode.Han
+	if r <= rune(0xfad9) {
+		c := uint16(r)
+		if len(t.R16) < 18 || c <= unicode.MaxLatin1 {
+			if c < 0x2e80 {
+				return false
+			}
+			if c <= 0x2e99 {
+				return true
+			}
+
+			if c < 0x2e9b {
+				return false
+			}
+			if c <= 0x2ef3 {
+				return true
+			}
+
+			if c < 0x2f00 {
+				return false
+			}
+			if c <= 0x2fd5 {
+				return true
+			}
+
+			if c < 0x3005 {
+				return false
+			}
+			if c <= 0x3007 {
+				return true
+			}
+
+			if c < 0x3021 {
+				return false
+			}
+			if c <= 0x3029 {
+				return true
+			}
+
+			if c < 0x3038 {
+				return false
+			}
+			if c <= 0x303b {
+				return true
+			}
+
+			if c < 0x3400 {
+				return false
+			}
+			if c <= 0x4dbf {
+				return true
+			}
+
+			if c < 0x4e00 {
+				return false
+			}
+			if c <= 0x9ffc {
+				return true
+			}
+
+			if c < 0xf900 {
+				return false
+			}
+			if c <= 0xfa6d {
+				return true
+			}
+
+			if c < 0xfa70 {
+				return false
+			}
+			if c <= 0xfad9 {
+				return true
+			}
+		}
+	}
 	return false
 }
---
+++ script_test.go
@@ -0,0 +1,37 @@
+package nlp
+
+import "testing"
+
+func TestScript(t *testing.T) {
+	t.Run("eval-rune", func(t *testing.T) {
+		r := '汉'
+		if !ScriptHan.EvaluateRune(r) {
+			t.FailNow()
+		}
+		r = 'Ю'
+		if !ScriptCyrillic.EvaluateRune(r) {
+			t.FailNow()
+		}
+	})
+}
+
+func BenchmarkScript(b *testing.B) {
+	b.Run("eval-rune", func(b *testing.B) {
+		b.ReportAllocs()
+		r := '汉'
+		for i := 0; i < b.N; i++ {
+			if !ScriptHan.EvaluateRune(r) {
+				b.FailNow()
+			}
+		}
+	})
+	b.Run("eval-rune1", func(b *testing.B) {
+		b.ReportAllocs()
+		r := '汉'
+		for i := 0; i < b.N; i++ {
+			if !ScriptHan.EvaluateRune1(r) {
+				b.FailNow()
+			}
+		}
+	})
+}
