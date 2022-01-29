7b5688d2551919a8636154f11e25d51a16e01975
Konstantin Voykov <koykov0@gmail.com>
2022-01-29 22:32:28 +0200

    Make sreBinary16/32 inline.

--- sre.go
+++ sre.go
@@ -18,17 +18,19 @@ type SRE struct {
 func sreEvalBinary16(ranges []unicode.Range16, r uint16) bool {
 	lo := 0
 	hi := len(ranges)
-	for lo < hi {
-		m := lo + (hi-lo)/2
-		rn := &ranges[m]
-		if rn.Lo <= r && r <= rn.Hi {
-			return rn.Stride == 1 || (r-rn.Lo)%rn.Stride == 0
-		}
-		if r < rn.Lo {
-			hi = m
-		} else {
-			lo = m + 1
-		}
+loop:
+	m := lo + (hi-lo)/2
+	rn := &ranges[m]
+	if rn.Lo <= r && r <= rn.Hi {
+		return rn.Stride == 1 || (r-rn.Lo)%rn.Stride == 0
+	}
+	if r < rn.Lo {
+		hi = m
+	} else {
+		lo = m + 1
+	}
+	if lo < hi {
+		goto loop
 	}
 	return false
 }
@@ -36,17 +38,19 @@ func sreEvalBinary16(ranges []unicode.Range16, r uint16) bool {
 func sreEvalBinary32(ranges []unicode.Range32, r uint32) bool {
 	lo := 0
 	hi := len(ranges)
-	for lo < hi {
-		m := lo + (hi-lo)/2
-		rn := ranges[m]
-		if rn.Lo <= r && r <= rn.Hi {
-			return rn.Stride == 1 || (r-rn.Lo)%rn.Stride == 0
-		}
-		if r < rn.Lo {
-			hi = m
-		} else {
-			lo = m + 1
-		}
+loop:
+	m := lo + (hi-lo)/2
+	rn := ranges[m]
+	if rn.Lo <= r && r <= rn.Hi {
+		return rn.Stride == 1 || (r-rn.Lo)%rn.Stride == 0
+	}
+	if r < rn.Lo {
+		hi = m
+	} else {
+		lo = m + 1
+	}
+	if lo < hi {
+		goto loop
 	}
 	return false
 }
