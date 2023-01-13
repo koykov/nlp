57039f08db9667794dd2280f85e38267485782ee
Konstantin Voykov <koykov0@gmail.com>
2023-01-13 23:53:17 +0200

    Ngram model: implement writer.

--- ngram_model.go
+++ ngram_model.go
@@ -1,8 +1,16 @@
 package nlp
 
 import (
+	"encoding/binary"
 	"io"
 	"sync"
+
+	"github.com/koykov/bytealg"
+)
+
+const (
+	ngmBlockSize = 4096
+	ngmBufSize   = 16384
 )
 
 type NgramModel struct {
@@ -65,18 +73,65 @@ func (m *NgramModel) AddFivegram(ng Fivegram) *NgramModel {
 	return m
 }
 
-func (m *NgramModel) Load(path string) error {
+func (m *NgramModel) LoadFile(path string) error {
 	_ = path
 	return nil
 }
 
 func (m *NgramModel) Write(w io.Writer) (int, error) {
+	w64 := func(dst []byte, v uint64) []byte {
+		off := len(dst)
+		dst = bytealg.GrowDelta(dst, 8)
+		binary.LittleEndian.PutUint64(dst[off:], v)
+		return dst
+	}
+
+	m.ul, m.bl, m.tl, m.ql, m.fl = uint64(len(m.u)), uint64(len(m.b)), uint64(len(m.t)), uint64(len(m.q)), uint64(len(m.f))
+
+	m.buf = m.buf[:0]
+	m.buf = w64(m.buf, m.v)
+	m.buf = w64(m.buf, m.ul)
+	m.buf = w64(m.buf, m.bl)
+	m.buf = w64(m.buf, m.tl)
+	m.buf = w64(m.buf, m.ql)
+	m.buf = w64(m.buf, m.fl)
+
 	_ = w
 	return 0, nil
 }
 
 func (m *NgramModel) Flush() error {
 	return nil
+}
+
+func (m *NgramModel) writeNG(w io.Writer, ng Unigram) (err error) {
+	off := len(m.buf)
+	m.buf = bytealg.GrowDelta(m.buf, 2)
+	binary.LittleEndian.PutUint16(m.buf[off:], uint16(ng))
+	if len(m.buf) > ngmBufSize {
+		err = m.flushBuf(w)
+	}
+	return
+}
+
+func (m *NgramModel) flushBuf(w io.Writer) (err error) {
+	p := m.buf
+	if len(p) == 0 {
+		return
+	}
+	for len(p) > ngmBlockSize {
+		if _, err = w.Write(p[:ngmBlockSize]); err != nil {
+			return
+		}
+		p = p[ngmBlockSize:]
+	}
+	if len(p) > 0 {
+		if _, err = w.Write(p[:ngmBlockSize]); err != nil {
+			return
+		}
+	}
+	m.buf = m.buf[:0]
+	return
 }
 
 func (m *NgramModel) init() {
