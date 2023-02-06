749cf111c14a53a7009e5be3ef85ec8fc17e143a
Konstantin Voykov <koykov0@gmail.com>
2023-02-06 22:19:59 +0200

    tokenizer: implement draft of tweet tokenizer.

---
+++ tokenize/casual.go
@@ -0,0 +1,35 @@
+package tokenize
+
+import (
+	"github.com/koykov/byteseq"
+	"github.com/koykov/nlp"
+)
+
+type TweetTokenizer[T byteseq.Byteseq] struct {
+	PreserveCase      bool
+	ReduceLen         bool
+	StripHandles      bool
+	MatchPhoneNumbers bool
+
+	o bool
+}
+
+func NewTweetTokenizer[T byteseq.Byteseq](preserveCase, reduceLen, stripHandles, matchPhoneNumbers bool) TweetTokenizer[T] {
+	return TweetTokenizer[T]{
+		PreserveCase:      preserveCase,
+		ReduceLen:         reduceLen,
+		StripHandles:      stripHandles,
+		MatchPhoneNumbers: matchPhoneNumbers,
+		o:                 true,
+	}
+}
+
+func (t TweetTokenizer[T]) Tokenize(x T) nlp.Tokens {
+	return t.AppendTokenize(nil, x)
+}
+
+func (t TweetTokenizer[T]) AppendTokenize(dst nlp.Tokens, x T) nlp.Tokens {
+	_ = x
+	// todo: implement me
+	return dst
+}
