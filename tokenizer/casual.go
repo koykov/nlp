dbd5e98fd361e661d6edf09d01dd72bc4b84df63
Konstantin Voykov <koykov0@gmail.com>
2023-03-15 15:41:07 +0200

    tokenizer: prepare regexp for tweet (casual) tokenizer.

--- tokenizer/casual.go
+++ tokenizer/casual.go
@@ -1,8 +1,17 @@
 package tokenizer
 
 import (
+	"regexp"
+
 	"github.com/koykov/byteseq"
 	"github.com/koykov/nlp"
+)
+
+var (
+	reEmoticons = regexp.MustCompile(`(?:[<>]?[:;=8][\-o*']?[)\](\[dDpP/:}{@|\\]|[)\](\[dDpP/:}{@|\\][\-o*']?[:;=8][<>]?|</?3)`)
+	reURL       = regexp.MustCompile(`(?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:[a-z]{2,13})/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s` + "`" + `!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:[a-z]{2,13})\b/?(?!@)`)
+	reFlag      = regexp.MustCompile("(?:[\U0001F1E6-\U0001F1FF]{2}|\U0001F3F4\U000E0067\U000E0062\U000E0065\U000E006e\U000E0067\U000E007F|\U0001F3F4\U000E0067\U000E0062\U000E0073\U000E0063\U000E0074\U000E007F|\U0001F3F4\U000E0067\U000E0062\U000E0077\U000E006C\U000E0073\U000E007F)")
+	rePhone     = regexp.MustCompile(`(?:(?:\+?[01][ *\-.)]*)?(?:[(]?\d{3}[ *\-.)]*)?\d{3}[ *\-.)]*\d{4})`)
 )
 
 type TweetTokenizer[T byteseq.Byteseq] struct {
