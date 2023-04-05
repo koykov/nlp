0fbe2e41e1bd134d73033579f95eee5e681deb01
Konstantin Voykov <koykov0@gmail.com>
2023-04-05 18:31:23 +0300

    Generator: add russian model generation.

--- generate.go
+++ generate.go
@@ -4,3 +4,4 @@ package nlp
 //go:generate nlpc --module=scripts --input=raw/scripts.json --target=scripts_repo.go
 
 //go:generate nlpc -module=ngmodels -input="https://github.com/koykov/dataset/blob/master/vocabulary/freelang/English.txt?raw=true" -target=~/dev/dataset/ngmodels/English.ngm
+//go:generate nlpc -module=ngmodels -input="https://github.com/koykov/dataset/blob/master/vocabulary/freelang/Russian.txt?raw=true" -target=~/dev/dataset/ngmodels/Russian.ngm
