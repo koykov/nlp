f1558b4e3480db2796d07c5da516a7560cab723f
Konstantin Voykov <koykov0@gmail.com>
2023-05-04 21:57:27 +0300

    languages: fix N languages description.

--- generate.go
+++ generate.go
@@ -1,6 +1,6 @@
 package nlp
 
-//go:generate nlpc --module=languages --input=raw/languages.json --target=languages_repo.go
+//go:generate nlpc --module=languages --input=raw/languages1.json --target=languages_repo.go
 //go:generate nlpc --module=scripts --input=raw/scripts.json --target=scripts_repo.go
 
 //go:generate nlpc -module=ngmodels -input="https://github.com/koykov/dataset/blob/master/vocabulary/freelang/English.txt?raw=true" -target=~/dev/dataset/ngmodels/English.ngm
